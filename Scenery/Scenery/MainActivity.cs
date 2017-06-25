67f9e2e79efa824e634b12f35902e3c46e43f91e
Ivan <lavrentyev.ivan@gmail.com>
2017-06-25 10:35:38 -0400

    no message

---
+++ Scenery/Scenery/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Runtime;
+using Android.Content;
+using Android.Views;
+using Android.Gms.Maps;
+using Android.Locations;
+using System;
+
+
+
+namespace Scenery
+{
+    [Activity(Label = "Scenery", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity, IOnMapReadyCallback
+    {
+        private GoogleMap mMap;
+        public LocationManager locMgr;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            locMgr = GetSystemService(Context.LocationService) as LocationManager;
+
+            SetUpMap();
+
+
+        }
+
+        private void SetUpMap()
+        {
+            if (mMap == null)
+            {
+                FragmentManager.FindFragmentById<MapFragment>(Resource.Id.map).GetMapAsync(this);
+            }
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            mMap = googleMap;
+        }
+
+    }
+}
+
